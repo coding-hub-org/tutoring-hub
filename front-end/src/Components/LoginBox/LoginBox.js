c79f8b73a123b1e873c997d854f39f7388f86863
winkhanh <giakhanhnk2000@gmail.com>
2018-11-17 14:16:40 -0500

    template login box

---
+++ front-end/src/Components/LoginBox/LoginBox.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './LoginBox.css';
+class LoginBox extends Component{
+    render(){
+        return (
+            <div id='login-box'>
+                <div id='login-picture'> 
+                    1
+                </div>     
+                <div id='login-form'>
+                    Login Form
+                </div>
+            </div>
+        );
+    }
+}
+export default LoginBox;
