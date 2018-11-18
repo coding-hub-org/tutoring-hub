69620406069cc9750d51204f0d53522ae3d9fe94
winkhanh <giakhanhnk2000@gmail.com>
2018-11-17 19:28:01 -0500

    Done with the Login Component, change the picture

--- front-end/src/Components/LoginBox/LoginBox.js
+++ front-end/src/Components/LoginBox/LoginBox.js
@@ -4,12 +4,25 @@ class LoginBox extends Component{
     render(){
         return (
             <div id='login-box'>
+                
                 <div id='login-picture'> 
                     1
                 </div>     
-                <div id='login-form'>
-                    Login Form
-                </div>
+                <form id='login-form' action="" method="post">
+                    <div id='login-form-title'>
+                        <strong>WELCOME TO TUTORING-HUB</strong>
+                    </div>
+                    <div id='login-form-username'>
+                        <input type='text' placeholder="Username" name="username" required></input>
+                    </div>
+                    <div id='login-form-password'>
+                        <input type='password' placeholder="Password" name="password" required></input>
+                    </div>
+                    <div id='login-form-button'>
+                        <button type="submit"><strong>LOGIN</strong></button>
+                    </div>
+                </form>
+                
             </div>
         );
     }
