e34c64159a2097a75e96a9b76582e851fe70ff8b
Gaurav Jayasawal <jgaurav6@gmail.com>
2019-01-30 19:02:38 -0500

    fixed khanhs name

--- front-end/src/Components/QuestionTile/QuestionTile.js
+++ front-end/src/Components/QuestionTile/QuestionTile.js
@@ -11,35 +11,45 @@ const QuestionTile = ({parameter, updateStats}) => {
         <div className={"questiontile-component"}>
             <p>{parameter}</p>            
             <div onChange={handleChange} className={"questiontile-component--wrapper"}>   
-                <label>
-                    <input type="radio" className={"option-input radio"} value="1" name={parameter} /> 1
+                    <input type="radio" className={"option-input radio"} value="1" name={parameter} />
+                <label for="1">
+                    1
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="2" name={parameter} /> 2
+                    <input type="radio" className={"option-input radio"} value="2" name={parameter} />
+                <label for="2">
+                    2
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="3" name={parameter} /> 3
+                    <input type="radio" className={"option-input radio"} value="3" name={parameter} />
+                <label for="3">
+                    3
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="4" name={parameter} /> 4
+                    <input type="radio" className={"option-input radio"} value="4" name={parameter} />
+                <label for="4">
+                    4
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="5" name={parameter} /> 5
+                    <input type="radio" className={"option-input radio"} value="5" name={parameter} />
+                <label for="5">
+                    5
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="6" name={parameter} /> 6
+                    <input type="radio" className={"option-input radio"} value="6" name={parameter} />
+                <label for="6">
+                    6
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="7" name={parameter} /> 7
+                    <input type="radio" className={"option-input radio"} value="7" name={parameter} />
+                <label for="7">
+                    7
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="8" name={parameter} /> 8
+                    <input type="radio" className={"option-input radio"} value="8" name={parameter} />
+                <label for="8">
+                    8
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="9" name={parameter} /> 9
+                    <input type="radio" className={"option-input radio"} value="9" name={parameter} />
+                <label for="9">
+                    9
                 </label>
-                <label>
-                    <input type="radio" className={"option-input radio"} value="10" name={parameter} /> 10
+                    <input type="radio" className={"option-input radio"} value="10" name={parameter} />
+                <label for="10">
+                    10
                 </label>
             </div>
         </div>
