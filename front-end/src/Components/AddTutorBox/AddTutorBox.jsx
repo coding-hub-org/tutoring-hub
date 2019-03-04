eff788634ab6aeb077510cdb36de9ab53d67a308
Michael Gates <michaelgatesdev@gmail.com>
2019-03-04 12:59:41 -0500

    Wrap button in anchor tag instead of wrapping anchor by button, so the whole button is clickable

--- front-end/src/Components/AddTutorBox/AddTutorBox.jsx
+++ front-end/src/Components/AddTutorBox/AddTutorBox.jsx
@@ -6,10 +6,12 @@ const AddTutorBox = () => {
     return (
         <div className={"add-tutor-box-component"}>
             <p>Missing someone?</p>
-            <div className="add-tutor-box-button">
-                <a href="/addtutor">Add Tutor</a>
-            </div>
-        </div>
+            <a href="/addtutor">
+                <div className="add-tutor-box-button">
+                    <p>Add Tutor</p>
+                </div>
+            </a>
+        </div >
     )
 }
 
