0c1467714a4ba50fc4e56f0359baa181f772b1b2
Hung Nguyen <nthungdev@gmail.com>
2019-03-07 15:13:19 -0500

    add Cloudinary api key

--- back-end/app.js
+++ back-end/app.js
@@ -1,50 +1,58 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var session = require('express-session');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
 
-var cors = require('cors')
+var cors = require("cors");
 
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var tutorsRouter = require("./routes/tutor");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var tutorsRouter = require('./routes/tutor');
-
-var loginRouter = require('./routes/login');
-var signupRouter = require('./routes/signup');
-var logoutRouter = require('./routes/logout');
-var settingRouter = require('./routes/setting');
+var loginRouter = require("./routes/login");
+var signupRouter = require("./routes/signup");
+var logoutRouter = require("./routes/logout");
+var settingRouter = require("./routes/setting");
 
 // Connect to mongoDB
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(cors())
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(session({ secret: "It's a new secret!" }));
 
 // Connect to database
-mongoose.connect('mongodb://codinghub:Plattsburgh#1@ds135577.mlab.com:35577/tutoringhub', { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb://codinghub:Plattsburgh#1@ds135577.mlab.com:35577/tutoringhub",
+  {
+    useNewUrlParser: true
+  }
+);
 mongoose.Promise = global.Promise;
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/tutor', tutorsRouter);
-app.use('/login', loginRouter);
-app.use('/signup', signupRouter);
-app.use('/logout', logoutRouter);
-app.use('/setting', settingRouter);
+// Cloudinary API Information
+const CLOUDINARY_API_KEY = "551819357444522";
+const CLOUDINARY_API_KEY_SECRET = "asGC9I6QtnXmPMSV8Y0EFV20dQU";
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/tutor", tutorsRouter);
+app.use("/login", loginRouter);
+app.use("/signup", signupRouter);
+app.use("/logout", logoutRouter);
+app.use("/setting", settingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,11 +63,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
