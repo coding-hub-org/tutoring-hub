e513ac39a0ec9e24047423d66038ead65bae5107
Michael Gates <michaelgatesdev@gmail.com>
2019-03-22 13:24:12 -0400

    Added the ability to upload tutor images

--- front-end/src/Components/ImageUpload/ImageUpload.jsx
+++ front-end/src/Components/ImageUpload/ImageUpload.jsx
@@ -11,20 +11,26 @@ class ImageUpload extends React.Component {
     }
 
     handleChange(event) {
-        var url = URL.createObjectURL(event.target.files[0]);
+        var selectedFile = event.target.files[0];
         this.setState({
-            file: url
+            file: selectedFile
         });
-        this.props.onChange(url);
+        this.props.onChange(selectedFile);
     }
 
     render() {
         return (
             <div className="image-upload">
-                <input type="file" onChange={this.handleChange} id={this.props.id} name={this.props.name} />
+                <input
+                    type="file"
+                    onChange={this.handleChange}
+                    id={this.props.id}
+                    name={this.props.name}
+                    accept="image/*"
+                />
                 <div className="image-preview-zone">
-                    {this.state.file && 
-                        <img src={this.state.file} className="image-preview" alt="Preview" />
+                    {this.state.file &&
+                        <img src={URL.createObjectURL(this.state.file)} className="image-preview" alt="Preview" />
                     }
                 </div>
             </div>
