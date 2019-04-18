608ff624cbb52febe55eccc8f3d7a801aca5cfc7
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-04-18 02:36:42 -0400

    Removed warnings

--- front-end/src/Sections/Review/Review.jsx
+++ front-end/src/Sections/Review/Review.jsx
@@ -1,143 +1,181 @@
-import React from 'react';
-import './Review.css';
+import React from "react";
+import "./Review.css";
 
-import NavBar from '../../Components/NavBar/NavBar';
-import Title from '../../Components/Title/Title';
-import QuestionTile from '../../Components/QuestionTile/QuestionTile';
-import Subheading from '../../Components/Subheading/Subheading';
+import NavBar from "../../Components/NavBar/NavBar";
+import Title from "../../Components/Title/Title";
+import QuestionTile from "../../Components/QuestionTile/QuestionTile";
+import Subheading from "../../Components/Subheading/Subheading";
 // import Course from '../../Components/Course/Course';
 
 class Review extends React.Component {
+	state = {
+		author: "Anonymous",
+		course: "BIO 102",
+		content: undefined,
+		methodology: undefined,
+		organization: undefined,
+		preparation: undefined,
+		clarity: undefined,
+		knowlege: undefined,
+		bookAgain: undefined
+	};
 
-    state = {
-        author: "Anonymous",
-        course: "BIO 102",
-        content: undefined,
-        methodology: undefined,
-        organization: undefined,
-        preparation: undefined,
-        clarity: undefined,
-        knowlege: undefined,
-        bookAgain: undefined,
-    }
+	handleClick = () => {
+		const {
+			author,
+			course,
+			content,
+			methodology,
+			organization,
+			preparation,
+			clarity,
+			knowlege,
+			bookAgain
+		} = this.state;
+		if (
+			author &&
+			course &&
+			content &&
+			methodology &&
+			organization &&
+			preparation &&
+			clarity &&
+			knowlege &&
+			bookAgain !== undefined
+		) {
+			// const url = this.props.match.url;
+			const data = {
+				author: author,
+				course: course,
+				content: content,
+				bookAgain: bookAgain,
+				statistics: {
+					methodology: methodology,
+					organization: organization,
+					preparation: preparation,
+					clarity: clarity,
+					knowlege: knowlege
+				}
+			};
+			fetch(`/api/v1${window.location.pathname}`.replace("/rate", ""), {
+				method: "PUT",
+				body: JSON.stringify(data), // data can be `string` or {object}!
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(res => res.json())
+				.then(response => {
+					console.log("Success");
+					document.getElementById("navbar").scrollIntoView();
+					window.location.href = "/";
+				})
+				.catch(error => console.error("Error:", error));
+		} else {
+			console.log("ERROR", this.state);
+		}
+	};
 
+	updateStats = score => {
+		const cathegory = score.target.name;
+		switch (cathegory) {
+			case "methodology":
+				this.setState({
+					methodology: Number(score.target.value)
+				});
+				break;
+			case "organization":
+				this.setState({
+					organization: Number(score.target.value)
+				});
+				break;
+			case "preparation":
+				this.setState({
+					preparation: Number(score.target.value)
+				});
+				break;
+			case "clarity":
+				this.setState({
+					clarity: Number(score.target.value)
+				});
+				break;
+			default:
+				this.setState({
+					knowlege: score.target.value
+				});
+				break;
+		}
+	};
 
-    handleClick = () => {
-        
-        const {author, course, content, methodology, organization, preparation, clarity, knowlege, bookAgain} = this.state;
-        if (author && course && content && methodology && organization && preparation && clarity && knowlege && bookAgain !== undefined) {
-            const url = this.props.match.url;
-            const data = {
-                "author": author,
-                "course": course,
-                "content": content,
-                "bookAgain" : bookAgain,
-                "statistics": {
-                    "methodology":  methodology,
-                    "organization":  organization,
-                    "preparation": preparation,
-                    "clarity":  clarity,
-                    "knowlege":  knowlege
-                }
-            }
-            fetch(`/api/v1${window.location.pathname}`.replace('/rate',''), {
-            method: 'PUT', 
-            body: JSON.stringify(data), // data can be `string` or {object}!
-            headers:{
-                'Content-Type': 'application/json'
-            }
-            }).then(res => res.json())
-            .then(response => {
-                console.log('Success');
-                document.getElementById("navbar").scrollIntoView();
-                window.location.href = "/";
-            })
-            .catch(error => console.error('Error:', error));
-        }
-        else {console.log("ERROR", this.state)};
-    }
+	handleComment = e => {
+		this.setState({
+			content: e.target.value
+		});
+	};
 
-    updateStats = (score) => {
-        const cathegory = score.target.name;
-        switch(cathegory) {
-            case 'methodology':
-                this.setState({
-                    methodology: Number(score.target.value)
-                });
-                break;
-            case 'organization':
-                this.setState({
-                    organization: Number(score.target.value)
-                });
-                break;
-            case 'preparation':
-                this.setState({
-                    preparation: Number(score.target.value)
-                });
-                break;
-            case 'clarity':
-                this.setState({
-                    clarity: Number(score.target.value)
-                });
-                break;
-            default:
-                this.setState({
-                    knowlege: score.target.value
-                });
-                break;
-        }
-    }
+	handleChange = e => {
+		const value = e.target.value === "yes" ? true : false;
+		this.setState({
+			bookAgain: value
+		});
+	};
 
-    handleComment = (e) => {
-        this.setState({
-            content: e.target.value
-        });
-    }
+	render() {
+		return (
+			<div className={"review-section"}>
+				<NavBar />
+				<div className={"review-section--wrapper"}>
+					<Title title={"Rate Gaurav"} />
 
-    handleChange = (e) => {
-        const value = (e.target.value === "yes") ? true : false;
-        this.setState({
-            bookAgain: value
-        });    
-    }
-    
-    render() {
-        
-        return(
-            <div className={'review-section'}>
-                <NavBar/>  
-                <div className={'review-section--wrapper'}>
-                    <Title title={'Rate Gaurav'}/>
+					<QuestionTile
+						parameter={"methodology"}
+						updateStats={this.updateStats}
+					/>
+					<QuestionTile
+						parameter={"organization"}
+						updateStats={this.updateStats}
+					/>
+					<QuestionTile
+						parameter={"preparation"}
+						updateStats={this.updateStats}
+					/>
+					<QuestionTile parameter={"clarity"} updateStats={this.updateStats} />
+					<QuestionTile parameter={"knowlege"} updateStats={this.updateStats} />
 
-                    <QuestionTile parameter={"methodology"} updateStats={this.updateStats} />
-                    <QuestionTile parameter={"organization"} updateStats={this.updateStats} />
-                    <QuestionTile parameter={"preparation"} updateStats={this.updateStats} />
-                    <QuestionTile parameter={"clarity"} updateStats={this.updateStats} />
-                    <QuestionTile parameter={"knowlege"} updateStats={this.updateStats} />
-
-                    <Subheading title={'About your session'}/>
-                    <div>
-                        <p>Would you book this tutor again? </p>
-                        <div onChange={this.handleChange} className={"review-section--bookagain"}>   
-                                <input type="radio" className={"option-input radio"} value="yes" name={"book-again"} />
-                            <label>
-                                YES
-                            </label>
-                                <input type="radio" className={"option-input radio"} value="no" name={"book-again"} />
-                            <label>
-                                NO
-                            </label>
-                        </div>
-                
-                    </div>
-                    <p>Class tutored </p>
-                    <Subheading title={'Comments'}/>
-                    <textarea onChange={this.handleComment} placeholder={"How was your session? Help this tutor to improve "}></textarea>
-                    <button onClick={this.handleClick}>SUBMIT REVIEW</button>
-                </div>        
-            </div>
-        );
-    }
+					<Subheading title={"About your session"} />
+					<div>
+						<p>Would you book this tutor again? </p>
+						<div
+							onChange={this.handleChange}
+							className={"review-section--bookagain"}
+						>
+							<input
+								type="radio"
+								className={"option-input radio"}
+								value="yes"
+								name={"book-again"}
+							/>
+							<label>YES</label>
+							<input
+								type="radio"
+								className={"option-input radio"}
+								value="no"
+								name={"book-again"}
+							/>
+							<label>NO</label>
+						</div>
+					</div>
+					<p>Class tutored </p>
+					<Subheading title={"Comments"} />
+					<textarea
+						onChange={this.handleComment}
+						placeholder={"How was your session? Help this tutor to improve "}
+					/>
+					<button onClick={this.handleClick}>SUBMIT REVIEW</button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Review;
