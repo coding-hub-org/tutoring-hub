9435872238c0cb18878736c8a96c6d4cdb6dbdf3
Michael Gates <michaelgatesdev@gmail.com>
2019-04-21 16:20:39 -0400

    Updated question tile to be more modular

--- front-end/src/Components/QuestionTile/QuestionTile.jsx
+++ front-end/src/Components/QuestionTile/QuestionTile.jsx
@@ -1,143 +1,100 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./QuestionTile.css";
 
-const QuestionTile = ({ parameter, updateStats }) => {
-	const handleChange = e => {
-		updateStats(e, e.target.value);
-	};
+class QuestionTile extends React.Component {
 
-	// const areas = [
-	//   "one",
-	//   "two",
-	//   "three",
-	//   "four",
-	//   "five",
-	//   "six",
-	//   "seven",
-	//   "eight",
-	//   "nine",
-	//   "ten"
-	// ];
+	constructor(props) {
+		super(props);
 
-	// const areasInput = [
-	//   "inputOne",
-	//   "inputTwo",
-	//   "inputThree",
-	//   "inputFour",
-	//   "inputFive",
-	//   "inputSix",
-	//   "inputSeven",
-	//   "inputEight",
-	//   "inputNine",
-	//   "inputTen"
-	// ];
+		this.state = {
+			selectedElementName: undefined,
+			value: -1,
+		};
 
-	// const getStyle = index => {
-	//   return {
-	//     gridArea: areas[index]
-	//   };
-	// };
+		this.onChange = this.onChange.bind(this);
+		this.onRightClick = this.onRightClick.bind(this);
+	}
 
-	return (
-		<div className={"questiontile-component"}>
-			<p>{parameter}</p>
-			<div
-				onChange={handleChange}
-				className={"questiontile-component--wrapper"}
-			>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="1"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="1">1</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="2"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="2">2</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="3"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="3">3</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="4"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="4">4</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="5"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="5">5</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="6"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="6">6</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="7"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="7">7</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="8"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="8">8</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="9"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="9">9</label>
-				</span>
-				<input
-					type="radio"
-					className={"option-input radio"}
-					value="10"
-					name={parameter}
-				/>
-				<span>
-					<label htmlFor="10">10</label>
-				</span>
+	onChange(e) {
+		this.triggerChange(e.target.name);
+	}
+
+	onRightClick(e) {
+		e.preventDefault();
+		this.clear();
+	}
+
+	getValueFromName(name) {
+		let value = name ?
+			name.replace(this.props.parameter + "-", "")
+			:
+			-1;
+		return value;
+	}
+
+	triggerChange(elementName) {
+		if (elementName) {
+			let elemValue = this.getValueFromName(elementName);
+			this.setState({
+				selectedElementName: elementName,
+				value: elemValue
+			});
+			this.props.onChange(this.props.parameter, elemValue);
+		}
+		else {
+			this.setState({
+				selectedElementName: undefined,
+				value: -1
+			});
+			this.props.onChange(this.props.parameter, -1);
+		}
+	}
+
+	clear() {
+		this.triggerChange(undefined);
+	}
+
+	isChecked(name) {
+		return this.state.selectedElementName === name;
+	}
+
+	render() {
+
+		let inputs = [];
+		for (let i = this.props.scaleMin; i <= this.props.scaleMax; i++) {
+			var input =
+				<Fragment
+					key={this.props.parameter + "-" + i}
+				>
+					<input
+						type="radio"
+						className={"option-input radio"}
+						value={i}
+						name={this.props.parameter + "-" + i}
+						onChange={this.onChange}
+						onContextMenu={this.onRightClick}
+						checked={this.isChecked(this.props.parameter + "-" + i)}
+					/>
+					<label
+						htmlFor={this.props.parameter + "-" + i}
+					>
+						{i}
+					</label>
+				</Fragment>
+			inputs.push(input);
+		}
+
+		return (
+			<div className={"QuestionTile-component"}>
+				<p>{this.props.parameter}</p>
+				<div
+					className={"QuestionTile-component--wrapper"}
+				>
+					{inputs}
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 export default QuestionTile;
