7c0a49b32ec48049f1b3d473a675eaa52ed8f4c8
Michael Gates <michaelgatesdev@gmail.com>
2019-04-21 17:03:17 -0400

    Moved 'yes/no' to its own component

---
+++ front-end/src/Components/YesNoInput/YesNoInput.jsx
@@ -0,0 +1,88 @@
+import React, { Fragment } from "react";
+import "./YesNoInput.css";
+
+class YesNoInput extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			selectedElementName: undefined,
+		};
+
+		this.onChange = this.onChange.bind(this);
+		this.onRightClick = this.onRightClick.bind(this);
+	}
+
+	onChange(e) {
+		this.triggerChange(e.target.name);
+	}
+
+	onRightClick(e) {
+		e.preventDefault();
+		this.clear();
+	}
+
+
+	triggerChange(elementName) {
+		if (elementName) {
+			this.setState({
+				selectedElementName: elementName,
+			});
+			this.props.onChange(elementName);
+		}
+		else {
+			this.setState({
+				selectedElementName: undefined,
+			});
+			this.props.onChange(undefined);
+		}
+	}
+
+	clear() {
+		this.triggerChange(undefined);
+	}
+
+	isChecked(name) {
+		return this.state.selectedElementName === name;
+	}
+
+	render() {
+
+		const inputs = this.props.choices.map(function (value, index) {
+			return (
+				<Fragment
+					key={value + "-" + index}
+				>
+					<input
+						type="radio"
+						className={"option-input radio"}
+						value={value}
+						name={value}
+						onChange={this.onChange}
+						onContextMenu={this.onRightClick}
+						checked={this.isChecked(value)}
+					/>
+					<label
+						htmlFor={value}
+					>
+						{value}
+					</label>
+				</Fragment>
+			);
+		}, this);
+
+		return (
+			<div className={"YesNoInput-component"}>
+				<p>{this.props.parameter}</p>
+				<div
+					className={"YesNoInput-component--wrapper"}
+				>
+					{inputs}
+				</div>
+			</div>
+		);
+	}
+}
+
+export default YesNoInput;
