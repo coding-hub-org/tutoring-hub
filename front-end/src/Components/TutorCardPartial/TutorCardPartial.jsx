fda5319cb084f4e31547bce02beb5c3ff296655d
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-04-21 13:39:36 -0400

    Finishied homepage reponsiveness

--- front-end/src/Components/TutorCardPartial/TutorCardPartial.jsx
+++ front-end/src/Components/TutorCardPartial/TutorCardPartial.jsx
@@ -1,62 +1,72 @@
-import React, { Component } from 'react'
-import './TutorCardPartial.css';
+import React, { Component } from "react";
+import "./TutorCardPartial.css";
 
-import { Link } from 'react-router-dom';
-import starRating from '../../Assets/rating-star.svg';
+import { Link } from "react-router-dom";
+import starRating from "../../Assets/rating-star.svg";
 
 class TutorCardPartial extends Component {
+	getFullName() {
+		return this.props.tutor.firstName + " " + this.props.tutor.lastName;
+	}
 
-    getFullName() {
-        return this.props.tutor.firstName + " " + this.props.tutor.lastName;
-    }
+	getAvg = ratings => {
+		let rating = 0,
+			total,
+			obj;
 
+		if (ratings.length === 0) return -1;
+		ratings.forEach(review => {
+			obj = review.statistics;
+			total =
+				(obj.methodology +
+					obj.organization +
+					obj.preparation +
+					obj.knowlege +
+					obj.clarity) /
+				5;
+			rating += total;
+		});
 
-    getAvg = (ratings) => {
-        let rating = 0, total, obj;
+		return rating / ratings.length;
+	};
 
-        if (ratings.length === 0) return -1;
-        ratings.forEach(review => {
-            obj = review.statistics;
-            total = (obj.methodology + obj.organization + obj.preparation + obj.knowlege + obj.clarity) / 5;
-            rating += total;
-        });
+	render() {
+		console.log(this.props.tutor);
+		const courses_list = this.props.tutor.courses
+			.slice(0, 7)
+			.map(course => <li>{course}</li>);
 
-        return rating / ratings.length;
-    }
+		return (
+			<div className={"Tutor-Card-Partial-Component"}>
+				<img
+					src={this.props.tutor.imageUrl}
+					alt={"Image of " + this.getFullName()}
+				/>
 
-    render() {
-        console.log(this.props.tutor);
-        const courses_list = this.props.tutor.courses.map((course) =>
-            <li>{course}</li>
-        )
+				<div className={"content"}>
+					<p className="full-name">{this.getFullName()}</p>
+					<div className="bio">
+						{courses_list}
+						{this.props.tutor.courses.length > 7
+							? "+" + (this.props.tutor.courses.length - 7).toString()
+							: ""}
+					</div>
 
-        return (
-            <div className={"Tutor-Card-Partial-Component"}>
-                <img src={this.props.tutor.imageUrl} alt={"Image of " + this.getFullName()} />
-
-                <div className={"content"}>
-                    <p className="full-name">{this.getFullName()}</p>
-                    <div className="bio">
-                        {courses_list}
-                    </div>
-
-                    <div className="bottom">
-                        <Link to={`/tutors/${this.props.tutor._id}`}>MORE</Link>
-                        <span className="rating">
-                            {
-                                (this.getAvg(this.props.tutor.reviews) === -1) ?
-                                    <span>N/A</span>
-                                    :
-                                    <span>{this.getAvg(this.props.tutor.reviews).toFixed(2)}</span>
-                            }
-                            <img src={starRating} alt="star rating" />
-                        </span>
-                    </div>
-                </div>
-            </div>
-        );
-
-    }
+					<div className="bottom">
+						<Link to={`/tutors/${this.props.tutor._id}`}>MORE</Link>
+						<span className="rating">
+							{this.getAvg(this.props.tutor.reviews) === -1 ? (
+								<span>N/A</span>
+							) : (
+								<span>{this.getAvg(this.props.tutor.reviews).toFixed(2)}</span>
+							)}
+							<img src={starRating} alt="star rating" />
+						</span>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default TutorCardPartial;
