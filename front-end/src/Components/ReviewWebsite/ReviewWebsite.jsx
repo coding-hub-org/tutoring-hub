2676134d398a6d809ea3a06697ce5506e0911064
votrungquan1999 <32785143+votrungquan1999@users.noreply.github.com>
2019-04-22 17:19:35 -0400

    minor changes

--- front-end/src/Components/ReviewWebsite/ReviewWebsite.jsx
+++ front-end/src/Components/ReviewWebsite/ReviewWebsite.jsx
@@ -4,7 +4,7 @@ import './ReviewWebsite.css';
 const ReviewWebsiteButton = () => {
     return (
         <button className="review-website">
-            <a href="https://forms.gle/kC1egakNaZScDtsr5" target="_blank">REVIEW</a>
+            <a href="https://forms.gle/kC1egakNaZScDtsr5" target="_blank">FEEDBACK</a>
         </button>
     )
 };
