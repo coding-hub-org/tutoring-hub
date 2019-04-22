133d2de4b9321e12439546d0b17c88bfe96fdb76
votrungquan1999 <32785143+votrungquan1999@users.noreply.github.com>
2019-04-22 16:03:41 -0400

    add_review_button

---
+++ front-end/src/Components/ReviewWebsite/ReviewWebsite.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ReviewWebsite.css';
+
+const ReviewWebsiteButton = () => {
+    return (
+        <a className="review-website" href="https://forms.gle/kC1egakNaZScDtsr5">REVIEW</a>
+    )
+};
+
+export default ReviewWebsiteButton;
