b743a3407570e7b2aa7de9ae0db225b2fc7dae4c
Michael Gates <michaelgatesdev@gmail.com>
2019-04-23 21:51:13 -0400

    More auth work

--- back-end/routes/api/v1/auth/index.js
+++ back-end/routes/api/v1/auth/index.js
@@ -3,18 +3,44 @@ const router = express.Router({
     mergeParams: true
 });
 
-router.get('/', function (req, res, next) {
-    res.send("This is where we check if the user is authenticated");
-});
-
-router.post('/', function (req, res, next) {
-    res.json(req.body);
-});
+const { OAuth2Client } = require('google-auth-library');
+const CLIENT_ID = '720087900394-emhkhqeh8m9nhq1mm07td42iuihbu56i.apps.googleusercontent.com';
+const client = new OAuth2Client(CLIENT_ID);
 
 
+async function verifyLogin(idToken) {
+    return new Promise((resolve, reject) => {
+        client.verifyIdToken({
+            idToken: idToken,
+            audience: CLIENT_ID,
+        })
+            .then(function (loginTicket) {
+                const payload = loginTicket.getPayload();
+                const userid = payload['sub'];
+                // If request specified a G Suite domain:
+                const domain = payload['hd'];
 
-router.post('/google', function (req, res, next) {
-    
+                console.log(domain);
+                return resolve();
+            })
+            .catch(function (err) {
+                return reject(err);
+            });
+    });
+}
+
+router.post('/google', async (req, res, next) => {
+    var user = req.body;
+
+    verifyLogin(user.idToken)
+        .then(function () {
+            console.log("Successfully verified the login!");
+        }).catch(function (err) {
+            console.log("There was an error verifying the login");
+            console.log(err);
+        });
+
+    res.json({});
 });
 
 
