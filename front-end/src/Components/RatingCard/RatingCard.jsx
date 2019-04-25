27d6b93305c96cf0cd6d9dc0a9245ddfa1c9f664
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-04-25 17:41:47 -0400

    Updated profile UI

--- front-end/src/Components/RatingCard/RatingCard.jsx
+++ front-end/src/Components/RatingCard/RatingCard.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
-import './RatingCard.css';
+import React from "react";
+import "./RatingCard.css";
+import ratingStar from "../../Assets/rating-star.svg";
 
-const RatingCard = ({reviews}) => {
-    
-    const getAvg = () => {
-        let rating = 0, total, obj;
+const RatingCard = ({ reviews }) => {
+	const getAvg = () => {
+		let rating = 0,
+			total,
+			obj;
 
-        if (reviews.length === 0) return -1;
-        reviews.forEach(review => {
-            obj = review.statistics;
-            total = (obj.methodology + obj.organization + obj.preparation + obj.knowlege + obj.clarity) / 5;
-            rating += total;
-        });
-        
-        return rating / reviews.length;
-    }
-    
-    return(
-        <div className={'ratingcard-component'}>
-            {
-                (getAvg() === -1) ?
-                <h1>N/A</h1> :
-                <h1>{getAvg().toFixed(2)}</h1>
-            }
-        </div>
-    );
+		if (reviews.length === 0) return -1;
+		reviews.forEach(review => {
+			obj = review.statistics;
+			total =
+				(obj.methodology +
+					obj.organization +
+					obj.preparation +
+					obj.knowlege +
+					obj.clarity) /
+				5;
+			rating += total;
+		});
+
+		return rating / reviews.length;
+	};
+
+	return (
+		<div className={"ratingcard-component"}>
+			<img src={ratingStar} alt="" />
+			{getAvg() === -1 ? <h1>N/A</h1> : <h1>{getAvg().toFixed(2)}</h1>}
+			<p>Overall</p>
+		</div>
+	);
 };
 
 export default RatingCard;
--- front-end/src/Components/ReviewCard/ReviewCard.jsx
+++ front-end/src/Components/ReviewCard/ReviewCard.jsx
@@ -1,45 +1,45 @@
 import React, { Component } from "react";
-import './ReviewCard.css';
+import "./ReviewCard.css";
 
 class ReviewCard extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
+		this.state = {};
+	}
 
-        };
-    }
+	render() {
+		const reviewList = this.props.tutor.reviews.map(review => {
+			let obj = review.statistics;
+			let total =
+				(obj.methodology +
+					obj.organization +
+					obj.preparation +
+					obj.knowlege +
+					obj.clarity) /
+				5;
 
-    render() {
-        const reviewList = this.props.tutor.reviews.map(review => {
-            let obj = review.statistics;
-            let total = (obj.methodology + obj.organization + obj.preparation + obj.knowlege + obj.clarity) / 5;
+			return (
+				<div className={"reviewcard-component"} key={review._id}>
+					<div className={"reviewcard-component--wrapper"}>
+						<div className={"reviewcard-component--wrapper__rating"}>
+							<p>RATING</p>
+							<h1>{total}</h1>
+						</div>
+						<div className={"reviewcard-component--wrapper__description"}>
+							<h1>{review.author}</h1>
+							<section className={"reviewcard-component--details"}>
+								<h3>{review.course}</h3>
+								<h3>{review.date.substring(0, 10)}</h3>
+							</section>
+							<p>{review.content}</p>
+						</div>
+					</div>
+				</div>
+			);
+		});
 
-            return (
-                <div className={"reviewcard-component"} key={review._id}>
-                    <div className={"reviewcard-component--wrapper"}>
-                        <div className={"reviewcard-component--wrapper__rating"}>
-                            <p>RATING</p>
-                            <h1>{total}</h1>
-                        </div>
-                        <div className={"reviewcard-component--wrapper__description"}>
-                            <h1>{review.author}</h1>
-                            <section className={"reviewcard-component--details"}>
-                                <h3>{review.course}</h3>
-                                <h3>{(review.date).substring(0, 10)}</h3>
-                            </section>
-                            <p>{review.content}</p>
-                        </div>
-                    </div>
-                </div>
-            )
-        });
-
-        return (
-            <div>
-                {reviewList}
-            </div>
-        );
-    }
+		return <div>{reviewList}</div>;
+	}
 }
 export default ReviewCard;
