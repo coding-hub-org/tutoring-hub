3a838c3b1a3aaeea65b3143e722289079b0a18b1
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-06-25 01:17:11 -0400

    Test github and discord

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -14,6 +14,7 @@ import BackToTopButton from "./Components/BackToTopButton/BackToTopButton";
 
 class App extends Component {
 	render() {
+		console.log("Test github with discord");
 		return (
 			<BrowserRouter>
 				<div className="App">
