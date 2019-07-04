7f65a73a8713daa2e005d5ef56abf086c359e095
Michael Gates <michaelgatesdev@gmail.com>
2019-07-04 14:11:43 -0400

    Made configurations more generic and flexible

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -8,182 +8,98 @@ import express, { NextFunction, Application } from 'express';
 import fetch from "node-fetch";
 
 import indexRoute from "./routes/index";
-
-import { createConfig, loadConfig } from "./config";
-
+import { ConfigManager, DatabaseConfig } from './config';
 
 
 class App {
 
-  // ------------------------------------------------------ \\
-  //              Configure express backend
-  // ------------------------------------------------------ \\
+    // ------------------------------------------------------ \\
+    //              Configure express backend
+    // ------------------------------------------------------ \\
 
-  public expressApp: Application = express();
+    public expressApp: Application = express();
 
-  constructor() {
-  }
-
-  public async initialize(): Promise<void> {
-
-    // Setup express stuff
-    console.debug("Setting up express server...");
-    this.setupExpress();
-    console.debug("Finished setting up express server.");
-
-
-    // setup database
-    createConfig("database-config.json", {
-      username: "",
-      password: "",
-      url: "",
-      useNewParser: true
-    }).then(function (created) {
-      if (created) {
-        console.log("Created default database config file. Edit it and restart the app.");
-        process.exit();
-        return;
-      }
-
-      loadConfig("database-config.json").then(function (loaded) {
-        let dbUsername = loaded.username;
-        let dbPassword = loaded.password;
-        let databaseURL = loaded.url
-          .replace("{username}", dbUsername)
-          .replace("{password}", dbPassword);
-        let useNewParser = loaded.useNewParser;
-
-        mongoose.connect(databaseURL, {
-          useNewUrlParser: useNewParser
-        }).then(function () {
-          mongoose.Promise = global.Promise;
-
-          var db = mongoose.connection;
-
-          //Bind connection to error event (to get notification of connection errors)
-          db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-          console.log("Successfully connected to the database");
-
-        }).catch(function (err) {
-          console.log("There was an error connecting to the database.");
-          console.log(err);
-          process.exit();
-          return;
-        });
-      }).catch(function (err) {
-        console.log("There was an error creating the database config file.");
-        console.log(err);
-        return;
-      });
-    });
-
-    // setup image CDN config
-    createConfig("image-cdn-config.json", {
-      cloud_name: "",
-      api_key: "",
-      api_secret: ""
-    }).then(function (created) {
-      if (created) {
-        console.log("Created default database config file. Edit it and restart the app.");
-        process.exit();
-        return;
-      }
-
-      loadConfig("image-cdn-config.json").then(function (loaded) {
-
-        let url = "https://API_KEY:API_SECRET@api.cloudinary.com/v1_1/CLOUD_NAME/resources/image"
-          .replace("API_KEY", loaded.api_key)
-          .replace("API_SECRET", loaded.api_secret)
-          .replace("CLOUD_NAME", loaded.cloud_name);
-
-        fetch(url).then(function (value) {
-          if (value.status == 200) {
-            cloudinary.config(loaded);
-            console.log("Successfully connected to the Image CDN");
-          }
-        }).catch(function (err) {
-          console.log("There was an error connecting to the Image CDN.");
-          console.log(err);
-          process.exit();
-        });
-      }).catch(function (err) {
-        console.log("There was an error creating the image config file.");
-        console.log(err);
-        process.exit();
-        return;
-      });
-    });
-
-  }
-
-  public setupExpress(): void {
-    console.debug("Setting up views");
-    this.setupViews();
-
-    console.debug("Using dev logger");
-    this.expressApp.use(logger('dev'));
-
-    console.debug("Setting up middleware");
-    this.setupMiddleware();
-
-    console.debug("Setting up static directories");
-    this.expressApp.use('/images', express.static("public/images"));
-    this.expressApp.use(express.static(path.join(__dirname, 'dist')));
-
-    console.debug("Setting up routes");
-    this.expressApp.use('/', indexRoute);
-
-    console.debug("Setting up static files to serve");
-    this.expressApp.use('*', (_req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
-
-    console.debug("Setting up error handling");
-    this.setupErrorHandling();
-  }
-
-  public setupViews(): void {
-    // view engine setup 
-    this.expressApp.set('views', path.join(__dirname, "../views"));
-    this.expressApp.set('view engine', 'ejs');
-  }
-
-  public setupMiddleware(): void {
-    this.expressApp.use(express.json());
-    this.expressApp.use(express.urlencoded({
-      extended: false
-    }));
-    this.expressApp.use(cookieParser());
-  }
-
-  public setupErrorHandling(): void {
-    //catch 404 and forward to error handler
-    this.expressApp.use((_req: express.Request, _res: express.Response, next: NextFunction) => {
-      next(createError(404));
-    });
-
-    // development error handler
-    // will print stacktrace
-    if (this.expressApp.get('env') === 'development') {
-
-      this.expressApp.use((err: any, _req: express.Request, res: express.Response, _next: NextFunction) => {
-        res.status(err['status'] || 500);
-        res.render('error', {
-          message: err.message,
-          error: err
-        });
-      });
+    constructor() {
     }
 
-    // production error handler
-    // no stacktraces leaked to user
-    this.expressApp.use((err: any, _req: express.Request, res: express.Response, _next: NextFunction) => {
-      res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: {}
-      });
-    });
-  }
+    public async initialize(): Promise<void> {
+
+        // Setup express stuff
+        console.debug("Setting up express server...");
+        this.setupExpress();
+        console.debug("Finished setting up express server.");
+
+        // database configuration
+        await new ConfigManager().initialize();
+    }
+
+    public setupExpress(): void {
+        console.debug("Setting up views");
+        this.setupViews();
+
+        console.debug("Using dev logger");
+        this.expressApp.use(logger('dev'));
+
+        console.debug("Setting up middleware");
+        this.setupMiddleware();
+
+        console.debug("Setting up static directories");
+        this.expressApp.use('/images', express.static("public/images"));
+        this.expressApp.use(express.static(path.join(__dirname, 'dist')));
+
+        console.debug("Setting up routes");
+        this.expressApp.use('/', indexRoute);
+
+        console.debug("Setting up static files to serve");
+        this.expressApp.use('*', (_req, res) => res.sendFile(path.join(__dirname, 'dist', 'index.html')));
+
+        console.debug("Setting up error handling");
+        this.setupErrorHandling();
+    }
+
+    public setupViews(): void {
+        // view engine setup 
+        this.expressApp.set('views', path.join(__dirname, "../views"));
+        this.expressApp.set('view engine', 'ejs');
+    }
+
+    public setupMiddleware(): void {
+        this.expressApp.use(express.json());
+        this.expressApp.use(express.urlencoded({
+            extended: false
+        }));
+        this.expressApp.use(cookieParser());
+    }
+
+    public setupErrorHandling(): void {
+        //catch 404 and forward to error handler
+        this.expressApp.use((_req: express.Request, _res: express.Response, next: NextFunction) => {
+            next(createError(404));
+        });
+
+        // development error handler
+        // will print stacktrace
+        if (this.expressApp.get('env') === 'development') {
+
+            this.expressApp.use((err: any, _req: express.Request, res: express.Response, _next: NextFunction) => {
+                res.status(err['status'] || 500);
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                });
+            });
+        }
+
+        // production error handler
+        // no stacktraces leaked to user
+        this.expressApp.use((err: any, _req: express.Request, res: express.Response, _next: NextFunction) => {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: {}
+            });
+        });
+    }
 
 }
 
--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -1,31 +1,137 @@
-var fs = require('fs');
+import fs from 'fs';
 
-export async function createConfig(fileName: string, defaultObj: any): Promise<any> {
-    return new Promise((resolve, reject) => {
-        if (!fs.exists(fileName, function (exists: any) {
-            if (exists) {
-                return resolve(false);
-            }
-            fs.writeFile(fileName, JSON.stringify(defaultObj, null, "\t"), (err: any) => {
-                if (err) {
-                    console.error(err);
-                    return reject(err);
-                };
-                return resolve(true);
-            });
-        }));
-    });
+interface Serializable<T> {
+    deserialize(input: Object): T;
 }
 
-export async function loadConfig(fileName: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-        fs.readFile(fileName, function (err: any, data: string) {
-            var config = JSON.parse(data);
-            if (!config || err) {
-                console.log(err);
-                return reject("There was an error loading the configuration file. (" + fileName + ")");
-            }
-            return resolve(config);
+export class ConfigManager {
+
+    private databaseConfig!: DatabaseConfig;
+
+    constructor() {
+    }
+
+    public async initialize() {
+        return new Promise(async (resolve, reject) => {
+            let self = this;
+
+            // Create database config
+            await ConfigManager.createIfNotExistsAndLoad<DatabaseConfig>(
+                'database.config.json',
+                DatabaseConfig,
+                ['database.config.json']
+            )
+                .then(function (resultObj: any) {
+                    if (resultObj.created) {
+                        console.log("Created database config!");
+                    }
+                    self.databaseConfig = resultObj.loaded;
+                    console.log("Loaded database config");
+                }).catch(function (err) {
+                    return reject(err);
+                });
+
+
+            console.log(this.databaseConfig.url);
+
+            return resolve();
         });
-    });
+    }
+
+    public static async createIfNotExists<T extends ConfigBase>(path: string, base: { new(...args: any[]): T; }, baseArgs: any[]): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            fs.promises.access(path, fs.constants.W_OK)
+                .then(function () {
+                    return resolve(false);
+                })
+                .catch(function (_err) {
+                    new base(...baseArgs).save()
+                        .then(function () {
+                            return resolve(true);
+                        }).catch(function (err) {
+                            return reject(err);
+                        });
+                });
+        });
+    }
+
+    public static async load<T extends ConfigBase>(path: string, base: { new(...args: any[]): T; }, baseArgs: any[]): Promise<ConfigBase> {
+        return new Promise((resolve, reject) => {
+            fs.promises.access(path, fs.constants.R_OK)
+                .then(function () {
+                    fs.promises.readFile(path, {
+                        encoding: 'utf8'
+                    })
+                        .then(function (rawData) {
+                            let data = JSON.parse(rawData);
+                            let deserialized: ConfigBase = new base(...baseArgs).deserialize(data);
+                            return resolve(deserialized);
+                        })
+                        .catch(function (err: any) {
+                            return reject(err);
+                        });
+                })
+                .catch(function (err: any) {
+                    return reject(err);
+                });
+        });
+    }
+
+    public static createIfNotExistsAndLoad<T extends ConfigBase>(path: string, base: { new(...args: any[]): T; }, baseArgs: any[]): Promise<{}> {
+        return new Promise((resolve, reject) => {
+            ConfigManager.createIfNotExists(path, base, baseArgs)
+                .then(function (created: boolean) {
+                    ConfigManager.load(path, base, baseArgs)
+                        .then(function (loaded: ConfigBase) {
+                            return resolve({ created: created, loaded: loaded });
+                        })
+                        .catch(function (err: any) {
+                            return reject(err);
+                        });
+                })
+                .catch(function (err: any) {
+                    return reject(err);
+                });
+        });
+    }
+}
+
+abstract class ConfigBase implements Serializable<ConfigBase> {
+
+    public configPath: string;
+
+    constructor(configPath: string) {
+        this.configPath = configPath;
+    }
+
+    public abstract deserialize(input: any): ConfigBase;
+
+    public async save(): Promise<void> {
+        let self = this;
+        return new Promise((resolve, reject) => {
+            // writes the file asynchronously with 4-spaced tabbing
+            fs.promises.writeFile(self.configPath, JSON.stringify(self, null, 4), null)
+                .then(function () {
+                    return resolve();
+                }).catch(function (err) {
+                    return reject(err);
+                });
+        });
+    }
+}
+
+
+export class DatabaseConfig extends ConfigBase {
+    public username: string = "";
+    public password: string = "";
+    public url: string = "mongodb://localhost/tutoring-hub";
+    public useNewUrlParser: boolean = true;
+
+    public deserialize(input: any): DatabaseConfig {
+        Object.keys(input).forEach(key => {
+            console.log(key);
+            this[key] = input[key];
+        });
+        return this;
+    }
 }
