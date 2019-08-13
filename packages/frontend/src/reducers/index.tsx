c1b9a8b02d9954ef0023e66e681cd075416e39d8
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-08-12 20:36:32 -0400

    Added lodash

--- packages/frontend/src/reducers/index.tsx
+++ packages/frontend/src/reducers/index.tsx
@@ -2,20 +2,6 @@ import { combineReducers } from "redux";
 import tutorReducer from "./tutorReducer";
 import homeReducer from "./homeReducer";
 
-// const initState = {
-// 	title: "All Tutors",
-// 	isLoading: true,
-// 	tutors: [],
-// 	courses: [],
-// 	filterName: "",
-// 	filterCourse: "",
-// 	filterRating: 0
-// };
-
-// const rootReducer = (state = initState, action: any) => {
-// 	return state;
-// };
-
 const rootReducer = combineReducers({
 	home: homeReducer,
 	tutor: tutorReducer
