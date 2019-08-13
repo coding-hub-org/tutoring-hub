d8bc94356e115c3e212d56ab9551a5c155b5a292
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-08-12 22:34:08 -0400

    Added helper file

--- packages/frontend/src/Components/SimpleTutorCard/index.tsx
+++ packages/frontend/src/Components/SimpleTutorCard/index.tsx
@@ -1,72 +1,69 @@
-import React from 'react';
-import './style.scss';
+import React from "react";
+import "./style.scss";
 
 interface Props {
-    editable?: boolean;
-    tutor?: any;
-    onUpdate?: any;
+	editable?: boolean;
+	tutor?: any;
+	onUpdate?: any;
 }
 
-interface State {
-
-}
+interface State {}
 
 export default class SimpleTutorCard extends React.Component<Props, State> {
+	constructor(props: Props) {
+		super(props);
 
-    constructor(props: Props) {
-        super(props);
+		this.removeTutor = this.removeTutor.bind(this);
+	}
 
-        this.removeTutor = this.removeTutor.bind(this);
-    }
+	removeTutor(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+		event.preventDefault();
 
-    getAvg = (ratings: any[]) => {
-        let rating = 0, total, obj;
+		fetch("/api/v1/cloudinary/delete/", {
+			method: "DELETE"
+		})
+			.then(() => {
+				console.log("Removed image with id " + this.props.tutor.imageID);
+				fetch("/api/v1/tutors/remove/" + this.props.tutor._id, {
+					method: "POST"
+				}).then(() => {
+					console.log("Removed tutor with id " + this.props.tutor._id);
+					this.props.onUpdate();
+				});
+			})
+			.catch(function(err) {
+				console.log(err);
+			});
+	}
 
-        if (ratings.length === 0) return -1;
-        ratings.forEach(review => {
-            obj = review.statistics;
-            total = (obj.methodology + obj.organization + obj.preparation + obj.knowledge + obj.clarity) / 5;
-            rating += total;
-        });
-
-        return rating / ratings.length;
-    }
-
-    removeTutor(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
-        event.preventDefault();
-
-        fetch('/api/v1/cloudinary/delete/', {
-            method: "DELETE"
-        }).then(() => {
-            console.log('Removed image with id ' + this.props.tutor.imageID);
-            fetch('/api/v1/tutors/remove/' + this.props.tutor._id, {
-                method: "POST"
-            }).then(() => {
-                console.log('Removed tutor with id ' + this.props.tutor._id);
-                this.props.onUpdate();
-            });
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
-
-    render() {
-        return (
-            <div className={"simple-tutor-card"}>
-                <img src={this.props.tutor.imageUrl} alt="" />
-                <div className={"simple-tutor-card-content"}>
-                    <div className="simple-tutor-card-title">{this.props.tutor.firstName + "  " + this.props.tutor.lastName}</div>
-                    {this.props.editable &&
-                        <div className="simple-tutor-card-controls">
-                            <ul>
-                                <li><span><a href="">Edit</a></span></li>
-                                <li><span><a href="" onClick={this.removeTutor}>Remove</a></span></li>
-                            </ul>
-                        </div>
-                    }
-                </div>
-            </div>
-        );
-
-    }
+	render() {
+		return (
+			<div className={"simple-tutor-card"}>
+				<img src={this.props.tutor.imageUrl} alt="" />
+				<div className={"simple-tutor-card-content"}>
+					<div className="simple-tutor-card-title">
+						{this.props.tutor.firstName + "  " + this.props.tutor.lastName}
+					</div>
+					{this.props.editable && (
+						<div className="simple-tutor-card-controls">
+							<ul>
+								<li>
+									<span>
+										<a href="">Edit</a>
+									</span>
+								</li>
+								<li>
+									<span>
+										<a href="" onClick={this.removeTutor}>
+											Remove
+										</a>
+									</span>
+								</li>
+							</ul>
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
---
+++ packages/frontend/src/helpers/tutorsHelper.tsx
@@ -0,0 +1,21 @@
+export const getAverageScore = (ratings: number[]) => {
+	let rating = 0,
+		total,
+		obj;
+
+	if (ratings.length === 0) return -1;
+
+	ratings.forEach((review: any) => {
+		obj = review.statistics;
+		total =
+			(obj.methodology +
+				obj.organization +
+				obj.preparation +
+				obj.knowledge +
+				obj.clarity) /
+			5;
+		rating += total;
+	});
+
+	return rating / ratings.length;
+};
